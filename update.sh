04b487e5991f266f4619bb1f8e6858ab43322aa8
Ink33 <Ink33@smlk.org>
2021-04-10 20:01:59 +0800

    refator: change gpg pubkey

--- update.sh
+++ update.sh
@@ -51,13 +51,12 @@ function GetOSName() {
 }
 
 Update
-#LatestVer="${GITHUB_REF#"refs/tags/"}"
-LatestVer="beta2"
-#git config --local user.name 'github-actions[bot]'
-#git config --local user.email '41898282+github-actions[bot]@users.noreply.github.com'
+LatestVer="${GITHUB_REF#"refs/tags/"}"
+git config --local user.name 'github-actions[bot]'
+git config --local user.email '41898282+github-actions[bot]@users.noreply.github.com'
 git add --all
 git commit -m "update to $LatestVer"
 git tag -d "$LatestVer"
 git tag "$LatestVer"
-#git push
-#git push --tags
+git push
+git push --tags
